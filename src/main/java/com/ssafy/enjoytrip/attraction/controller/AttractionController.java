331d836d3e2d879157934307d1d2744c831cafdb
thdqudgns <thdqudgns9612@gmail.com>
2023-04-28 17:37:33 +0900

    [ADD] 관광지 목록 조회 추가 230428 1737

--- src/main/java/com/ssafy/enjoytrip/attraction/controller/AttractionController.java
+++ src/main/java/com/ssafy/enjoytrip/attraction/controller/AttractionController.java
@@ -1,15 +1,25 @@
 package com.ssafy.enjoytrip.attraction.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller
-//@RequestMapping("/attraction")
-public class AttractionController {
+import com.ssafy.enjoytrip.attraction.model.service.AttractionService;
 
-	@GetMapping("/")
-	public String index() {
-		return "index";
+@Controller
+@RequestMapping("/attraction")
+public class AttractionController {
+	
+	private AttractionService attractionService;
+	public AttractionController(AttractionService attractionService) {
+		this.attractionService = attractionService;
 	}
+
+	@GetMapping("/list")
+	public String list(Model model) throws Exception {
+		model.addAttribute("list", attractionService.getAttractionList());
+		return "attraction/list";
+	}
+
 }
